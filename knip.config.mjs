87961e27f0ae6c852f9a24edf2f50ebd4f4ae492
oclio <hello@oclio.dev>
2026-08-29 14:35:40 +0200

    chore(setup): add knip for dead code and unused dependency detection

---
+++ knip.config.mjs
@@ -0,0 +1,9 @@
+const knipConfig = {
+  $schema: 'https://unpkg.com/knip@6/schema.json',
+  ignore: [],
+  ignoreBinaries: [],
+  ignoreDependencies: [],
+  tags: ['-lintignore'],
+};
+
+export default knipConfig;
