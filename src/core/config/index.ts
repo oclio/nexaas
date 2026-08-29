4884a26cc99b16886f1e7b4484aa511f6f100b5c
oclio <hello@oclio.dev>
2026-08-29 14:09:22 +0200

    chore(setup): scaffold nexaas boilerplate structure

---
+++ src/core/config/index.ts
@@ -0,0 +1,6 @@
+export const app = {
+  title: 'nexaas',
+  description: 'A scalable, production-ready SaaS boilerplate built for high performance and rapid prototyping in Next.js.',
+  logo: '/images/logo.svg',
+  emailLogo: '/images/logo.png',
+};
