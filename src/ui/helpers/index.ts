82ab9f1757a83e1d046afb67ab09107cc4b89d1f
oclio <hello@oclio.dev>
2026-09-02 17:56:08 +0200

    feat(ui): add typography and section utilities

--- src/ui/helpers/index.ts
+++ src/ui/helpers/index.ts
@@ -4,3 +4,31 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function handleHashScroll(
+  event_: React.MouseEvent<HTMLAnchorElement>,
+  href: string,
+  pathname: string,
+  callback?: () => void,
+) {
+  if (!href.includes('#')) {
+    return;
+  }
+
+  const id = href.split('#', 2)[1];
+  if (!id) {
+    return;
+  }
+
+  const isLandingPage = pathname === '/' || /^\/[a-z]{2}$/.test(pathname);
+  const element = document.querySelector(`#${id}`);
+
+  if (element && isLandingPage) {
+    event_.preventDefault();
+    element.scrollIntoView({ behavior: 'smooth' });
+    history.pushState(undefined, '', `#${id}`);
+    if (callback) {
+      callback();
+    }
+  }
+}
