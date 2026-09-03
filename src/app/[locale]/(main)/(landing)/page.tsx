1f433443f2fd56fb3ccc55580984e5bcdce7cf2d
oclio <hello@oclio.dev>
2026-09-03 17:31:06 +0200

    revert(landing): restore landing sections
    This reverts commit a6262b2.

--- src/app/[locale]/(main)/(landing)/page.tsx
+++ src/app/[locale]/(main)/(landing)/page.tsx
@@ -1,13 +1,13 @@
 import { createPageMetadata } from '@/core/seo';
 
-// import CtaSection from './_components/cta-section';
-// import FaqSection from './_components/faq-section';
-// import FeaturesSection from './_components/features-section';
+import CtaSection from './_components/cta-section';
+import FaqSection from './_components/faq-section';
+import FeaturesSection from './_components/features-section';
 import { HashScroll } from './_components/hash-scroll';
 import HeroSection from './_components/hero-section';
-// import PricingSection from './_components/pricing-section';
-// import StatsSection from './_components/stats-section';
-// import WhatIsIncluded from './_components/what-is-included-section';
+import PricingSection from './_components/pricing-section';
+import StatsSection from './_components/stats-section';
+import WhatIsIncluded from './_components/what-is-included-section';
 
 export async function generateMetadata({
   params,
@@ -21,12 +21,12 @@ export default async function LandingPage() {
     <div className="flex min-h-svh w-full flex-col">
       <HashScroll />
       <HeroSection />
-      {/* <StatsSection />
+      <StatsSection />
       <FeaturesSection />
       <PricingSection />
       <FaqSection />
       <WhatIsIncluded />
-      <CtaSection /> */}
+      <CtaSection />
     </div>
   );
 }
